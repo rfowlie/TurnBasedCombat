7ddad2e2c5fa6e6e931785e5630cffe0f29d38ea
rfowlie <robertanthonyfowlie@gmail.com>
2025-05-16 14:17:58 -0700

    Update Finding Connect N Logic
    White box game functioning well, time design and implement formal rules and win conditions, various AI behaviours (should do it in the style of risk where you can pick the number of AI oppenents and then select their play style)
    Should try to introduce ART aspects

--- Source/TurnBasedCombat/Private/_Basic/Grid/GridShapeUtility.cpp
+++ Source/TurnBasedCombat/Private/_Basic/Grid/GridShapeUtility.cpp
@@ -66,3 +66,175 @@ void UGridShapeUtility::CheckConfiguration(
 		}
 	}
 }
+
+void UGridShapeUtility::GetShapeConfiguration_Line(TArray<FShapeConfiguration>& Configurations,
+	const EGridDirection GridDirection, int32 Length)
+{
+	if (Length < 2) { return; }
+	if (Length > 10)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Excessive Value: %d, could cause issues so clamping..."), Length);
+		Length = FMath::Clamp(Length, 2, 10);
+	}
+
+	switch (GridDirection)
+	{
+	case Row:
+		for (int32 StartPos = 1 - Length; StartPos < 1; StartPos++)
+		{
+			FShapeConfiguration TempConfiguration;
+			for (int32 CurrentPos = 0; CurrentPos < Length; CurrentPos++)
+			{
+				TempConfiguration.ShapeConfiguration.Add(FGridPosition(0, StartPos + CurrentPos));
+			}
+			Configurations.Add(TempConfiguration);
+		}
+		break;
+	case Column:
+		for (int32 StartPos = 1 - Length; StartPos < 1; StartPos++)
+		{
+			FShapeConfiguration TempConfiguration;
+			for (int32 CurrentPos = 0; CurrentPos < Length; CurrentPos++)
+			{
+				TempConfiguration.ShapeConfiguration.Add(FGridPosition(StartPos + CurrentPos, 0));
+			}
+			Configurations.Add(TempConfiguration);
+		}
+		break;
+	case Diagonal_TopDown:
+		// high to low
+		for (int32 StartPos = 1 - Length; StartPos < 1; StartPos++)
+		{
+			FShapeConfiguration TempConfiguration;
+			for (int32 CurrentPos = 0; CurrentPos < Length; CurrentPos++)
+			{
+				TempConfiguration.ShapeConfiguration.Add(FGridPosition(StartPos + CurrentPos, StartPos + CurrentPos));
+			}
+			Configurations.Add(TempConfiguration);
+		}
+		break;
+	case Diagonal_BottomUp:
+		// low to high
+		for (int32 StartPos = 1 - Length; StartPos < 1; StartPos++)
+		{
+			FShapeConfiguration TempConfiguration;
+			for (int32 CurrentPos = 0; CurrentPos < Length; CurrentPos++)
+			{
+				TempConfiguration.ShapeConfiguration.Add(FGridPosition(StartPos + CurrentPos, -(StartPos + CurrentPos)));
+			}
+			Configurations.Add(TempConfiguration);
+		}
+		break;
+	}
+}
+
+void UGridShapeUtility::GetLongestConnect(
+	TArray<FGridPosition>& LongestConnect, const EGridDirection GridDirection,
+	const FGridPosition& StartingPosition, const TArray<FGridPosition>& GridPositions,
+	int32 Min, int32 Max)
+{
+	if (Min < 2 || Max > 10 || Max < Min)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Min and Max values are invalid... (Min:%d) (Max:%d)"), Min, Max);
+		return;
+	}
+	
+	if (!GridPositions.Contains(StartingPosition)) { return; }
+	TMap<FGridPosition, bool> GridMap;
+	for (auto GridPosition : GridPositions) { GridMap.Add(GridPosition, false); }
+
+	int32 RowDirectionMultiplier = 0;
+	int32 ColumnDirectionMultiplier = 0;
+	
+	switch (GridDirection){
+	case Row:
+		ColumnDirectionMultiplier = 1;
+		break;
+	case Column:
+		RowDirectionMultiplier = 1;
+		break;
+	case Diagonal_TopDown:
+		RowDirectionMultiplier = 1;
+		ColumnDirectionMultiplier = 1;
+		break;
+	case Diagonal_BottomUp:
+		RowDirectionMultiplier = -1;
+		ColumnDirectionMultiplier = 1;
+		break;
+	}
+	
+	for (int32 Index = 1 - Max; Index < Max; Index++)
+	{
+		FGridPosition TempPosition (
+			Index * RowDirectionMultiplier, Index * ColumnDirectionMultiplier);
+		if (GridMap.Contains(StartingPosition + TempPosition))
+		{
+			LongestConnect.Add(TempPosition);
+		}
+		else
+		{
+			if (Index > 0)
+			{
+				break;
+			}
+			
+			LongestConnect.Empty();
+		}
+	}
+
+	if (LongestConnect.Num() < Min) { LongestConnect.Empty(); }
+}
+
+void UGridShapeUtility::GetLongestLine(
+	TArray<FGridPosition>& OutGridPositions, const TArray<FGridPosition>& InGridPositions,
+	const FGridPosition& StartingPosition, const EGridDirection GridDirection)
+{
+	if (!InGridPositions.Contains(StartingPosition)) { return; }
+	TMap<FGridPosition, bool> GridMap;
+	for (auto GridPosition : InGridPositions) { GridMap.Add(GridPosition, false); }
+
+	int32 RowDirectionMultiplier = 0;
+	int32 ColumnDirectionMultiplier = 0;
+	
+	switch (GridDirection){
+	case Row:
+		ColumnDirectionMultiplier = 1;
+		break;
+	case Column:
+		RowDirectionMultiplier = 1;
+		break;
+	case Diagonal_TopDown:
+		RowDirectionMultiplier = 1;
+		ColumnDirectionMultiplier = 1;
+		break;
+	case Diagonal_BottomUp:
+		RowDirectionMultiplier = -1;
+		ColumnDirectionMultiplier = 1;
+		break;
+	}
+	
+	int32 Positive = 0;
+	for (int32 Index = 1; Index <= MAX_int32; Index++)
+	{
+		FGridPosition TempPosition (
+			Index * RowDirectionMultiplier, Index * ColumnDirectionMultiplier);
+		if (!GridMap.Contains(StartingPosition + TempPosition)) { break; }
+		Positive = Index;
+	}
+	
+	int32 Negative = 0;
+	for (int32 Index = 1; Index <= MAX_int32; Index++)
+	{
+		FGridPosition TempPosition (
+			-Index * RowDirectionMultiplier, -Index * ColumnDirectionMultiplier);
+		if (!GridMap.Contains(StartingPosition + TempPosition)) { break; }
+		Negative = -Index;
+	}
+
+	for (int32 Index = Negative; Index <= Positive; Index++)
+	{
+		OutGridPositions.Add(
+			StartingPosition +
+			FGridPosition(Index * RowDirectionMultiplier, Index * ColumnDirectionMultiplier));
+	}	
+}
--- Source/TurnBasedCombat/Public/_Basic/Grid/GridShapeUtility.h
+++ Source/TurnBasedCombat/Public/_Basic/Grid/GridShapeUtility.h
@@ -20,6 +20,14 @@ struct FShapeConfiguration
 	TArray<FGridPosition> ShapeConfiguration;
 };
 
+UENUM(BlueprintType)
+enum EGridDirection
+{
+	Row,
+	Column,
+	Diagonal_TopDown,
+	Diagonal_BottomUp
+};
 
 /**
  * map out different shapes on an XY grid
@@ -37,4 +45,20 @@ public:
 	static void CheckConfiguration(
 		const FGridPosition StartingPosition, const TArray<FGridPosition>& GridPositions,
 		const TArray<FShapeConfiguration>& ConfigurationsToCheck, TArray<FShapeConfiguration>& OutSuccessfulConfigurations);
+
+	UFUNCTION(BlueprintCallable)
+	static void GetShapeConfiguration_Line(TArray<FShapeConfiguration>& Configurations, EGridDirection GridDirection, int32 Length);
+
+	// given a grid direction find the longest connection of positions within a set min and max
+	UFUNCTION(BlueprintCallable)
+	static void GetLongestConnect(
+		TArray<FGridPosition>& LongestConnect, const EGridDirection GridDirection,
+		const FGridPosition& StartingPosition, const TArray<FGridPosition>& GridPositions,
+		int32 Min, int32 Max);
+	
+	UFUNCTION(BlueprintCallable)
+	static void GetLongestLine(
+		TArray<FGridPosition>& OutGridPositions, const TArray<FGridPosition>& InGridPositions,
+		const FGridPosition& StartingPosition, const EGridDirection GridDirection);
+
 };
