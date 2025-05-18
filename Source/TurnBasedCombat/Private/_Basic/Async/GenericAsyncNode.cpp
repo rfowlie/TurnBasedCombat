8f8fcdd31256e8e2892d9fb768327cfc9f9ae9c1
rfowlie <robertanthonyfowlie@gmail.com>
2025-05-18 03:22:20 -0700

    Add Static Meshes and Update Game Logic

---
+++ Source/TurnBasedCombat/Private/_Basic/Async/GenericAsyncNode.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "_Basic/Async/GenericAsyncNode.h"
+
+UGenericAsyncNode* UGenericAsyncNode::GenericAsyncNode()
+{
+	UGenericAsyncNode* Node = NewObject<UGenericAsyncNode>();
+	return Node;
+}
+
+void UGenericAsyncNode::Activate()
+{
+	(new FAutoDeleteAsyncTask<FGenericNonAbandonableTask>(this))->StartBackgroundTask();
+}
+
+FGenericNonAbandonableTask::FGenericNonAbandonableTask(UGenericAsyncNode* GenericAsyncNode)
+{
+	CallingObject = GenericAsyncNode;
+}
+
+FGenericNonAbandonableTask::~FGenericNonAbandonableTask()
+{
+	CallingObject->OnComplete.Broadcast();
+	CallingObject->SetReadyToDestroy();
+}
+
+void FGenericNonAbandonableTask::DoWork()
+{
+	CallingObject->Task.Broadcast();
+}
---
+++ Source/TurnBasedCombat/Public/_Basic/Async/GenericAsyncNode.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GenericAsyncNode.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTaskOutput);
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEDCOMBAT_API UGenericAsyncNode : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FTaskOutput Task;
+
+	UPROPERTY(BlueprintAssignable)
+	FTaskOutput OnComplete;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UGenericAsyncNode* GenericAsyncNode();
+
+	virtual void Activate() override;
+	
+};
+
+//============================================================================================
+
+class FGenericNonAbandonableTask : public FNonAbandonableTask
+{
+public:
+	
+	FGenericNonAbandonableTask(UGenericAsyncNode* GenericAsyncNode);
+
+	~FGenericNonAbandonableTask();
+
+	//UE5 needs this
+	FORCEINLINE TStatId GetStatId() const
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(FGenericNonAbandonableTask, STATGROUP_ThreadPoolAsyncTasks);
+	}
+
+	UGenericAsyncNode* CallingObject = nullptr;
+
+	void DoWork();
+
+};
