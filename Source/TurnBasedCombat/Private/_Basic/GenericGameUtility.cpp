7c9f3ed16a8b22a3f590d37a28131fd75543d05f
rfowlie <robertanthonyfowlie@gmail.com>
2025-05-20 19:57:33 -0700

    Add Top-N Selection Logic
    Could use some tweaking to accomodate the game style as there is more transparancy between good and bad moves

---
+++ Source/TurnBasedCombat/Private/_Basic/GenericGameUtility.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "_Basic/GenericGameUtility.h"
+
+
+void UGenericGameUtility::MakeObjectScoreArray(TArray<FObjectScore>& OutObjectScores, const TMap<UObject*, int32>& ScoreMap)
+{
+	for (const auto& Elem : ScoreMap)
+	{
+		OutObjectScores.Add(FObjectScore(Elem.Key, Elem.Value));		
+	}
+
+	OutObjectScores.Sort();
+}
---
+++ Source/TurnBasedCombat/Public/_Basic/GenericGameUtility.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GenericGameUtility.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FObjectScore
+{
+	GENERATED_BODY()
+
+	FObjectScore() {}
+	FObjectScore(UObject* InObject, const int32 InScore) : Object(InObject), Score(InScore) {}
+	FObjectScore(const FObjectScore& Other) : Object(Other.Object), Score(Other.Score) {}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UObject* Object = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Score = 0;
+
+	bool operator==(const FObjectScore& Other) const
+	{
+		return Score == Other.Score;
+	}
+	
+	auto operator<(const FObjectScore& Other) const
+	{
+		return Score < Other.Score;
+	}
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEDCOMBAT_API UGenericGameUtility : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void MakeObjectScoreArray(TArray<FObjectScore>& OutObjectScores, const TMap<UObject*, int32>& ScoreMap);
+
+};
