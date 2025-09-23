15552b2cde12e381d3fd3bcdbc69abbe9921d1ea
rfowlie <robertanthonyfowlie@gmail.com>
2025-09-22 22:16:42 -0700

    Stashed Work

---
+++ Plugins/UnrealAIMechanics/Source/UnrealAIMechanics/Public/Utility/AI_UtilityFocusBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "AI_UtilityFocusBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class UNREALAIMECHANICS_API UAI_UtilityFocusBase : public UObject
+{
+	GENERATED_BODY()
+};
